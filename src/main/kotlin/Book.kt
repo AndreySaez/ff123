801e6814fa429763e34ac8c67bc5298404709a64
AndreySaez <116518026+AndreySaez@users.noreply.github.com>
2022-12-08 23:49:30 +0400

    Update src/main/kotlin/Book.kt
    Co-authored-by: imadFndt <57181892+imadFndt@users.noreply.github.com>

--- src/main/kotlin/Book.kt
+++ src/main/kotlin/Book.kt
@@ -1,10 +1,10 @@
 class Book(override val wordCount: Int, override val price: Int) : Publication {
     override fun getType() {
-        if (wordCount <= 1000) {
-            println("Flash Fiction")
-        } else if (wordCount <= 7500) {
-            println("Short Story")
-        } else println("Novel")
+        when {
+            wordCount <= 1000 -> println("Flash Fiction")
+            wordCount <= 7500 -> println("Short Story")
+            else -> println("Novel")
+        }
     }
 
 }
